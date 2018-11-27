0f85be7aeed4f88a0dbdbbd178b47100b996179e
fullPint <davila.alec@gmail.com>
2018-11-26 18:32:46 -0600

    built out ideal low pass class

---
+++ Filters/ideal_low_pass.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def IdealLowPass:
+    def __init__(self, shape, cutoff):
+        self.p = shape[0]
+        self.q = shape[1]
+        self.cutoff = cutoff
+        self.filter = self.build_filter()
+
+    def build_filter(self):
+        filter = np.zeroes((self.p, self.q))
+        index_iterator = np.nditer(filter, flags=['multi_index'])
+        while not index_iterator.finished:
+            u = index_iterator.multi_index[0]
+            v = index_iterator.multi_index[1]
+            filter[u][v] = 1 if self.calculate_distance(u, v) > self.cutoff else 0
+            index_iterator.iternext()
+        return filter
+
+    def calculate_distance(self, u, v):
+        dist = np.sqrt(np.square(u - (self.p/2)) + np.square(v - (self.q/2)))
+        return dist
+
+    def get_filter(self):
+        return self.filter
