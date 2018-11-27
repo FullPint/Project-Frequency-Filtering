ffbc06affdbda7f7e67a0aae44b619d7acc47b30
fullPint <davila.alec@gmail.com>
2018-11-26 18:29:24 -0600

    renamed vars

--- Filters/laplace_filter.py
+++ Filters/laplace_filter.py
@@ -7,12 +7,12 @@ class Laplace:
         self.filter = self.build_filter()
 
     def build_filter(self):
-        h = np.zeroes((self.p, self.q))
+        filter = np.zeroes((self.p, self.q))
         index_iterator = np.nditer(filter, flags=['multi_index'])
         while not index_iterator.finished:
             u = index_iterator.multi_index[0]
             v = index_iterator.multi_index[1]
-            h[u][v] = 1 + (np.square(u - self.p) + np.square(v - self.q))
+            filter[u][v] = 1 + (np.square(u - self.p) + np.square(v - self.q))
             index_iterator.iternext()
         return filter
 
